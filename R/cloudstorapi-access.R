f4524a4a88a314a6c73f95b2f1e51c160f184157
Philip Parker <philipparker@192-168-1-10.tpgi.com.au>
2021-06-10 10:20:14 +1000

    Cloud_meta function added

--- R/cloudstorapi-access.R
+++ R/cloudstorapi-access.R
@@ -68,3 +68,54 @@ cloud_put <- function(file_name, local_file, user=cloud_auth_user(), password=cl
   in_path = path.expand(local_file)
   httr::PUT(uri, body = httr::upload_file(in_path), config = httr::authenticate(user, password))
 }
+
+
+#' cloud_meta
+#'
+#' @param user Your cloudstor username
+#' @param password Your cloudstor password
+#' @param cloud_address cloudstor webDav address to the location you want to save on cloudstor
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cloud_meta <- function(user=cloud_auth_user(), password=cloud_auth_pwd(), cloud_address = "https://cloudstor.aarnet.edu.au/plus/remote.php/webdav/") {
+  uri <- utils::URLencode(cloud_address)
+  # fetch directory listing via curl and parse XML response
+  h <- curl::new_handle()
+  curl::handle_setopt(h, customrequest = "PROPFIND")
+  curl::handle_setopt(h, username = user)
+  curl::handle_setopt(h, password = password)
+  response <- curl::curl_fetch_memory(uri, h)
+  text <- rawToChar(response$content)
+  doc <- XML::xmlParse(text, asText=TRUE)
+  # calculate relative paths
+  base <- paste(paste("/", strsplit(uri, "/")[[1]][-1:-3], sep="", collapse=""), "/", sep="")
+  files <- unlist(
+    XML::xpathApply(doc, "//d:response/d:href", function(node) {
+      sub(base, "", utils::URLdecode(XML::xmlValue(node)), fixed=TRUE)
+    })
+  )
+  modified <- unlist(
+    XML::xpathApply(doc, "//d:getlastmodified", function(node) {
+      sub(base, "", utils::URLdecode(XML::xmlValue(node)), fixed=TRUE)
+    })
+  )
+  size <- unlist(
+    XML::xpathApply(doc, "//d:quota-used-bytes", function(node) {
+      sub(base, "", utils::URLdecode(XML::xmlValue(node)), fixed=TRUE)
+    })
+  )
+  tag <- unlist(
+    XML::xpathApply(doc, "//d:getetag", function(node) {
+      sub(base, "", utils::URLdecode(XML::xmlValue(node)), fixed=TRUE)
+    })
+  )
+
+  result <- data.frame(file_name = files, tag = tag,
+                       file_modified = modified,
+                       file_size = size)
+  return(result)
+}
+
