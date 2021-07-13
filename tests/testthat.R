80f552c10c4a465cc384ebcecdb1038d3663b854
Taren Sanders <taren.sanders@acu.edu.au>
2021-07-13 14:21:10 +1000

    Add tests and update docs

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
 library(testthat)
 library(cloudstoR)
+library(mockery)
 
 test_check("cloudstoR")
--- tests/testthat/test-cloudstorapi-auth.R
+++ tests/testthat/test-cloudstorapi-auth.R
@@ -1,5 +1,3 @@
-context("Authentication")
-
 test_that("Save and restore keys", {
   # TODO
   testthat::expect_equal(TRUE, TRUE)
