1892d5123e23412cd0cd60068ef1ee59261f32c8
Taren Sanders <taren.sanders@acu.edu.au>
2021-09-18 18:01:59 +1000

    Fix the dumb mistake in the env skip

--- R/helper.R
+++
@@ -1,6 +0,0 @@
-skip_if_no_envs <- function(){
-  testthat::skip_if(nchar(Sys.getenv("CLOUD_USER",unset = FALSE))==0,
-                    "Environment variable not available")
-  testthat::skip_if(nchar(Sys.getenv("CLOUD_PASS",unset = FALSE))==0,
-                    "Environment variable not available")
-}
--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,6 @@
 skip_if_no_envs <- function(){
-  testthat::skip_if(nchar(Sys.getenv("CLOUD_USER",unset = FALSE))==0,
+  testthat::skip_if(nchar(Sys.getenv("CLOUD_USER",unset = ""))==0,
                     "Environment variable not available")
-  testthat::skip_if(nchar(Sys.getenv("CLOUD_PASS",unset = FALSE))==0,
+  testthat::skip_if(nchar(Sys.getenv("CLOUD_PASS",unset = ""))==0,
                     "Environment variable not available")
 }
